55ee0371539945b0625bd1a532a2279346b82923
diksha-kalra <kalradiksha11@gmail.com>
2020-11-10 19:51:51 +0530

    Added: using regex pattern to validate name of employee and throwing exception

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,14 +1,17 @@
+const nameRegexPattern=RegExp('^[A-Z]{1}[a-z]{2,}$');
 class EmployeePayrollData{
     //property
     id;
     salary;
     gender;
     startDate;
-
 //constructor
 constructor(...params) {
     this.id=params[0];
-    this.name=params[1];
+    if(!nameRegexPattern.test(params[1]))throw "invalid employee name"
+    {
+            this.name=params[1];
+    }
     this.salary=params[2];
     this.gender=params[3];
     this.startDate=params[4];
@@ -16,7 +19,7 @@ constructor(...params) {
 
 //getter and setter
 get name() { return this._name;}
-set name(name) {this._name=name;}
+set name(name){ this._name=name;}
 
 //method
 toString(){
@@ -25,9 +28,11 @@ toString(){
     return "Id: "+this.id+" Name: "+this.name+" salary: "+this.salary+" Gender: "+this.gender+" Start Date: "+empDate;
 }
 }
-let employeePayrollData=new EmployeePayrollData(1,"Mark",3000000);
+try{
+let employeePayrollData=new EmployeePayrollData(1,"mark",3000000);
 console.log(employeePayrollData.toString());
-employeePayrollData.name="John";
-console.log(employeePayrollData.toString());
+}catch(e){
+    console.log(e);
+}
 let newemployeePayrollData=new EmployeePayrollData(2,"Terrisa",4000000,"F",new Date());
 console.log(newemployeePayrollData.toString());
