5f80968def0eeee1ab51e8a0de1ec5b6b15207d2
diksha-kalra <kalradiksha11@gmail.com>
2020-11-10 19:15:14 +0530

    Added: extend employeepayroll data class to have gender and startdate property

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,12 +2,16 @@ class EmployeePayrollData{
     //property
     id;
     salary;
+    gender;
+    startDate;
 
 //constructor
-constructor(id,name,salary) {
-    this.id=id;
-    this.name=name;
-    this.salary=salary;
+constructor(...params) {
+    this.id=params[0];
+    this.name=params[1];
+    this.salary=params[2];
+    this.gender=params[3];
+    this.startDate=params[4];
 }
 
 //getter and setter
@@ -16,10 +20,14 @@ set name(name) {this._name=name;}
 
 //method
 toString(){
-    return "Id: "+this.id+" Name: "+this.name+" salary: "+this.salary;
+    const options={ year: 'numeric', month: 'long', day: 'numeric'};
+    const empDate=(!this.startDate)?"undefined": this.startDate.toLocaleDateString("en-US",options);
+    return "Id: "+this.id+" Name: "+this.name+" salary: "+this.salary+" Gender: "+this.gender+" Start Date: "+empDate;
 }
 }
 let employeePayrollData=new EmployeePayrollData(1,"Mark",3000000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name="John";
 console.log(employeePayrollData.toString());
+let newemployeePayrollData=new EmployeePayrollData(2,"Terrisa",4000000,"F",new Date());
+console.log(newemployeePayrollData.toString());
