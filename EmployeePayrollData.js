01f498b499872bdac07e3aeab17cf8e610f5ba02
diksha-kalra <kalradiksha11@gmail.com>
2020-11-10 20:12:42 +0530

    Added: using regex patterns to check employee id, salary, gender and date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,4 +1,5 @@
 const nameRegexPattern=RegExp('^[A-Z]{1}[a-z]{2,}$');
+const genderRegexPattern=RegExp('[M,F]{1}$')
 class EmployeePayrollData{
     //property
     id;
@@ -7,14 +8,26 @@ class EmployeePayrollData{
     startDate;
 //constructor
 constructor(...params) {
-    this.id=params[0];
+    if(!params[0]>0) throw "invalid employee id"
+    {
+        this.id=params[0];
+    }
     if(!nameRegexPattern.test(params[1]))throw "invalid employee name"
     {
             this.name=params[1];
     }
-    this.salary=params[2];
-    this.gender=params[3];
-    this.startDate=params[4];
+    if(!params[2]>0) throw "invalid employee salaray must be greater than 0"
+    {
+        this.salary=params[2];
+    }
+    if(!genderRegexPattern.test(params[3])) throw "invalid gender type"
+    {
+        this.gender=params[3];
+    }
+    if(params[4]>new Date()) throw "future date not allowed"
+    {
+        this.startDate=params[4];
+    }
 }
 
 //getter and setter
@@ -28,11 +41,19 @@ toString(){
     return "Id: "+this.id+" Name: "+this.name+" salary: "+this.salary+" Gender: "+this.gender+" Start Date: "+empDate;
 }
 }
+
 try{
-let employeePayrollData=new EmployeePayrollData(1,"mark",3000000);
-console.log(employeePayrollData.toString());
+    let invalidDateEmployeePayrollData=new EmployeePayrollData(3,"Mark",3000000,"M",new Date("December 10, 2020"));
+    console.log(invalidDateEmployeePayrollData.toString());
+
 }catch(e){
     console.log(e);
 }
-let newemployeePayrollData=new EmployeePayrollData(2,"Terrisa",4000000,"F",new Date());
-console.log(newemployeePayrollData.toString());
+try{
+    let invalidIdemployeePayrollData=new EmployeePayrollData(0,"Terrisa",4000000,"F",new Date());
+    console.log(invalidIdemployeePayrollData.toString());
+}catch(e){
+    console.log(e);
+}
+let employeePayrollData=new EmployeePayrollData(3,"Charles",5000000,"M",new Date());
+console.log(employeePayrollData.toString());
